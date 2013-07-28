95482d64ebb762736ab76be0bb408c3cd8dc6d4f
Raj Girimaji <rajgirimaji@Rajs-MacBook-Pro.local>
2013-07-28 13:55:14 -0400

    checking in only the java file

--- src/com/example/calculator2/MainActivity.java
+++ src/com/example/calculator2/MainActivity.java
@@ -15,39 +15,40 @@ public class MainActivity extends Activity {
 
 	Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0;//number digit
 	Button btnPlus,btnMinus,btnTimes,btnClear,btnEqual;//number digit
-	 double numberOne=0;
-	 double numberTwo=0;
+	double numberOne=0;
+	double numberTwo=0;
 	double numberResult=0;
 	TextView textview;
-	
+
 	boolean userIsInMiddleOfTypingANumber = false;
+	String operationEntered = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		MyNumberListener myNumberListner = new MyNumberListener();
 		MyOperationListener myOperationListner = new MyOperationListener();
 		cleanListener clean = new cleanListener();
-		
+
 		setContentView(R.layout.activity_main);
 		textview= (TextView)findViewById(R.id.txtResult);
-		btn1=(Button)findViewById(R.id.button9);
-		btn2=(Button)findViewById(R.id.button10);
-		btn3=(Button)findViewById(R.id.button11);
-		btn4=(Button)findViewById(R.id.button5);
-		btn5=(Button)findViewById(R.id.button6);
-		btn6=(Button)findViewById(R.id.button7);
-		btn7=(Button)findViewById(R.id.button1);
-		btn8=(Button)findViewById(R.id.button2);
-		btn9=(Button)findViewById(R.id.button3);
-		btn0=(Button)findViewById(R.id.button14);
-		btnPlus=(Button)findViewById(R.id.button12);
-		btnMinus=(Button)findViewById(R.id.button8);
-		btnTimes=(Button)findViewById(R.id.button4);
-		btnClear=(Button)findViewById(R.id.button13);
-		btnEqual=(Button)findViewById(R.id.button15);
-		
-		
+		btn1=(Button)findViewById(R.id.btnOne);
+		btn2=(Button)findViewById(R.id.btnTwo);
+		btn3=(Button)findViewById(R.id.btnThree);
+		btn4=(Button)findViewById(R.id.btnFour);
+		btn5=(Button)findViewById(R.id.btnFive);
+		btn6=(Button)findViewById(R.id.btnSix);
+		btn7=(Button)findViewById(R.id.btnSeven);
+		btn8=(Button)findViewById(R.id.btnEight);
+		btn9=(Button)findViewById(R.id.btnNine);
+		btn0=(Button)findViewById(R.id.btnZero);
+		btnPlus=(Button)findViewById(R.id.btnPlus);
+		btnMinus=(Button)findViewById(R.id.btnMinus);
+		btnTimes=(Button)findViewById(R.id.btnMultiply);
+		btnClear=(Button)findViewById(R.id.btnClear);
+		btnEqual=(Button)findViewById(R.id.btnEqual);
+
+
 		btn1.setOnClickListener(myNumberListner);
 		btn2.setOnClickListener(myNumberListner);
 		btn3.setOnClickListener(myNumberListner);
@@ -63,7 +64,7 @@ public class MainActivity extends Activity {
 		btnTimes.setOnClickListener(myOperationListner);
 		btnClear.setOnClickListener(clean);
 		btnEqual.setOnClickListener(myOperationListner);
-		
+
 	}
 
 	@Override
@@ -74,49 +75,111 @@ public class MainActivity extends Activity {
 	}
 	class cleanListener implements OnClickListener
 	{
-
 		@Override
 		public void onClick(View v) {
 			textview.setText("");
-			
+
 		}
 	}
-	 class MyNumberListener implements OnClickListener
-	    {
-			@Override
-			public void onClick(View arg0) {	
-				Button whichButton = (Button) arg0;
-				int number = Integer.parseInt(whichButton.getText().toString());
-				if(!userIsInMiddleOfTypingANumber)
+	class MyNumberListener implements OnClickListener
+	{
+		@Override
+		public void onClick(View arg0) {	
+			Button whichButton = (Button) arg0;
+			String strNumberEntered = whichButton.getText().toString();
+			int numberAsInteger = Integer.parseInt(strNumberEntered);
+			if(!userIsInMiddleOfTypingANumber)
+			{
+				numberOne = numberAsInteger;
+				userIsInMiddleOfTypingANumber = true;	
+				textview.setText(strNumberEntered);
+			}
+			else
+			{
+				String oldNumber;
+				String newNumber;
+				oldNumber = textview.getText().toString() ;
+				newNumber = oldNumber + strNumberEntered;
+				textview.setText(newNumber);
+
+			}
+		}
+
+	}
+	class MyOperationListener implements OnClickListener
+	{
+		@Override
+		public void onClick(View arg0) {	
+			Button whichButton = (Button) arg0;
+			operationEntered = whichButton.getText().toString();
+			if(userIsInMiddleOfTypingANumber)
+			{
+				if(operationEntered.equals("="))
 				{
-					numberOne = number;
-				  userIsInMiddleOfTypingANumber = true;	
-				  textview.setText(Double.toString(numberOne));
+					performMath();
+					return;
+				}
+				String existingText = textview.getText().toString() ;
+				String newText = existingText + operationEntered;
+				textview.setText(newText);
+			}
+		}
+	}
+	
+	void performMath()
+	{
+		String textEnteredSoFar = textview.getText().toString();
+		int length = textEnteredSoFar.length();
+		String value1 = new String();
+		String value2 = new String();
+		char operand = ' ';
+		boolean value1IsDone = false;
+		for(int i=0; i<length; i++)
+		{
+			char currentCharacter = textEnteredSoFar.charAt(i);
+			if(currentCharacter == '+')
+			{
+				operand = currentCharacter;
+				value1IsDone = true;
+			}
+			else if(currentCharacter == '-')
+			{
+				value1IsDone = true;
+				operand = currentCharacter;
+				//doSubstraction();
+			}
+			else if(currentCharacter == '*')
+			{
+				value1IsDone = true;
+				operand = currentCharacter;
+				//doMultiplication();
+			}
+			else
+			{
+				if(value1IsDone)
+				{
+					value2 += String.valueOf(currentCharacter);
 				}
 				else
 				{
-					String oldNumber;
-					String newNumber;
-					
-					oldNumber = textview.getText().toString() ;
-					newNumber = oldNumber +Integer.toString(number);
-					textview.setText(newNumber);
-					
+					value1 += String.valueOf(currentCharacter);
 				}
 			}
-			
-	    }
-	    class MyOperationListener implements OnClickListener
-	    {
-			@Override
-			public void onClick(View arg0) {	
-				Button whichButton = (Button) arg0;
-				whichButton.getText();
-				Toast.makeText(MainActivity.this,
-						"Am clicked " + whichButton.getText(), 
-						Toast.LENGTH_LONG).show();
-			}
-	    	
-	    }
+		}
+		
+		if(operand == '+')
+		{
+			doAddition(value1, value2);
+		}
+	}
+	
+	void doAddition(String value1, String value2)
+	{
+		int number1 = Integer.parseInt(value1);
+		int number2 = Integer.parseInt(value2);
+		
+		int sum = number1 + number2;
+		textview.setText(String.valueOf(sum));
+	}
 
 }
